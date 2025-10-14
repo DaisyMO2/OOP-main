4c69372fd3993b3a2315bcf04f2c5af3017f4b18
DaisyMO2 <marungadaizy10@gmail.com>
2025-10-14 02:06:49 -0700

    new

---
+++ class revision/revision.py
@@ -0,0 +1,37 @@
+#N0.1
+a = 5
+b = 2.5
+result = a + b
+print("result")
+print(type("result"))
+
+#N0.2
+x = 10
+y = 3.14
+z = x * y
+print(z)
+print(type("z"))
+
+#N0.3
+a = 7
+b = float(a) 
+print(b)
+print(type(b))
+
+#N0.4
+a = "42"
+b = int(a)
+print(a)
+print(int(b))
+
+#N0.5
+a = 15
+b = 3
+c = a / b
+print(c)
+print(type(c))
+d = int(c)
+print(d)
+print(type(d))
+
+
