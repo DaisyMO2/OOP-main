1e20a153058573e1e60cb457dfb7248fb85a8134
DaisyMO2 <marungadaizy10@gmail.com>
2025-10-28 17:35:33 +0300

    exercise

---
+++ gadgets.py
@@ -0,0 +1,21 @@
+class Gadget:
+    def start(self):
+        print("Gadget started")
+        
+        
+class Phone(Gadget):
+    def start(self):
+        print("Phone is starting")
+        
+class Laptop(Gadget):
+    def start(self):
+        print("Laptop is starting")
+        
+ 
+gadgets = [Phone(), Laptop()]
+  
+for gadget in gadgets:
+    gadget.start()      
+
+        
+    
