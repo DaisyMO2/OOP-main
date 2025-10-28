e59631966718273dc54cfb94ec7cf67b7ffa5cf5
DaisyMO2 <marungadaizy10@gmail.com>
2025-10-28 08:47:33 +0300

    gadgets

--- class revision/car.py
+++ class revision/car.py
@@ -1,7 +1,12 @@
 class car:
-    def __init__(self, name, year):
+    def __init__(self, name, year, color):
         self.name = name
         self.year = year
+        self.color=color
         
-    print "self".name(name):  
+    def drive (self):
+        return f"{self.name}"
+    def get_info(self):
+         return(f"the{self.name} of color {self.color} made in {self.year}")
+     
     
