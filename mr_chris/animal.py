9b03bc15fe5bee8b98720b92651db7da803e33d1
DaisyMO2 <marungadaizy10@gmail.com>
2025-11-04 10:04:45 +0300

    animal

--- mr_chris/animal.py
+++ mr_chris/animal.py
@@ -1,20 +1,20 @@
 class Animal:
     def speak(self):
         print("animal is talking")
-        
+
 class Dog(Animal):
     def wroof(self):
         print("dog is barking")
-        
-class cat(Animal):
+
+class Cat(Animal):
     def meow(self):
-        print("move bitch")
-        
+        print("cat is meowing")
+
 animal = Animal()
 animal.speak()
-animal2 = Dog()
-animal3 = cat()
-Animals = [Dog(), cat()]       
+
+Animals = [Dog(), Cat()]       
 for a in Animals:
     a.speak()
+
 
