49613d84cd73121ba1aae321d09e355b477bc06e
DaisyMO2 <marungadaizy10@gmail.com>
2025-11-04 10:02:49 +0300

    mmm

--- mr_chris/shape.py
+++ mr_chris/shape.py
@@ -1,21 +1,19 @@
 class Shape:
     def __init__(self, area):
-        area = area
-        
-class rectangle(Shape):
-    def __init__(self, l, w):
-        self.l = 8
-        self.w = 4
-        print("l * w")
-        
-class circle(Shape):
-    def __init__(self, r, pi):
-        self.r = 5
-        self.pi = 3.14
-        print("pi * r^2")
-        
-items = [rectangle(8*4), circle(5*3.14)]
-for item in items:
-    print(item)
+        self.area = area
 
-        
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        area = l * w
+        super().__init__(area)
+        print(area)
+
+class Circle(Shape):
+    def __init__(self, r, pi=3.14):
+        area = pi * (r ** 2)
+        super().__init__(area)
+        print(area)
+
+items = [Rectangle(8, 4), Circle(5)]
+for item in items:
+    print(item.area)
