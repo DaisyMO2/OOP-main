460dd35339cfd23e621f987753a54cd0a202d302
DaisyMO2 <marungadaizy10@gmail.com>
2025-11-11 18:08:12 +0300

    Update Hotel.py

--- Assignment_1/Hotel.py
+++ Assignment_1/Hotel.py
@@ -5,7 +5,7 @@ class Hotel:
         self.location = location
 
 hotel1 = Hotel("Kyaninga Lodge", 5, "Kyaninga")
-hotel2 = Hotel("Mountains of the Moon Hotel", 4, "Fort Portal Town")
+hotel2 = Hotel("Mountains of the Moon Hotel", 4, "F-Portal Town")
 hotel3 = Hotel("Nyaika Hotel", 4, "Fort Portal Town")
 hotel4 = Hotel("Dutchess Hotel", 3, "Fort Portal")
 hotel5 = Hotel("Rwenzori View Guesthouse", 3, "Fort Portal")
