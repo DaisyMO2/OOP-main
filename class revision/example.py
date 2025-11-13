546e9c214a5b896b324c009a6b8a4152b5669750
DaisyMO2 <marungadaizy10@gmail.com>
2025-11-13 21:48:44 +0300

    Update example.py

--- class revision/example.py
+++ class revision/example.py
@@ -1,7 +1,7 @@
 class student:
     def __init__(self,name):
         self.name = name             #public
-        self._gpa = 3.5               #protected(has one underscore)
+        self._gpa = 3.79               #protected(has one underscore)
         self.__password = "1234"       #private(has 2 underscores)
         
 student_one =  student("daisy")
